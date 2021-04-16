a28e3b7c7ed5f4d7b811c3c6b778155219dd7a01
priyanshikhetwani <63781869+priyanshikhetwani@users.noreply.github.com>
2021-04-16 18:33:20 +0530

    Update About.js

--- src/MyComponents/About.js
+++ src/MyComponents/About.js
@@ -7,7 +7,7 @@ export const About = () => {
     }
     return (
         <div className="text-center" style={styles}>
-        <h3 className="mb-3 mt-2">Manage and Visualize yur todos'</h3>
+        <h3 className="mb-3 mt-2">Manage and Visualize your todos'</h3>
         <div>
             TodoList is a beautifully simple, free todo list,
             task list that will help keep your busy life organised everyday. 
