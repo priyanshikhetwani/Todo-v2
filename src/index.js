034eecf150505bd07fca05a2bc140e4009d89a6e
priyanshikhetwani <63781869+priyanshikhetwani@users.noreply.github.com>
2021-04-16 17:01:40 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {
+  BrowserRouter as Router,
+  Route
+} from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
+  <Router basename={process.env.PUBLIC_URL + '/'}>
     <App />
+  </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
